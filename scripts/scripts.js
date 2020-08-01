d0637eb0b14de8faffceed2749cdaced62f62bcf
fohlle <patte2000@hotmail.com>
2020-08-01 17:13:49 +0200

    5 rounds, score

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,41 +11,83 @@ function computerPlay(){
     }
 }
 
-function playerSelection(choice){
-    if(choice === "rock"){
-        return "rock";
-    }else if(choice == "paper"){
-        return "paper";
-    }else if(choice == "scissors"){
-        return "scissors";
+function playerSelection(){
+    let x = prompt("rock, paper, scissors");
+    while(true){
+        if(x === "rock"){
+            break;
+        }else if(x === "paper"){
+            break;
+        }else if(x === "scissors"){
+            break;
+        }
+         x = prompt("try again");
     }
+    return x.toLocaleLowerCase();
 }
+
+
 
 function playRound(player, comp){
     console.log("Player: " + player + " Computer: " + comp);
     if(comp === "rock" && player === "scissors"){
-        return "you lose rock beats scissors";
+        console.log("you lose rock beats scissors");
+        return 1;
     }
     if(comp === "rock" && player === "rock"){
-        return "Draw";
+        console.log("Draw");
+        return 0;
     }
     if(comp === "paper" && player === "rock"){
-        return "you lose paper beats rock";
+        console.log("you lose paper beats rock");
+        return 1;
     }
     if(comp === "paper" && player === "paper"){
-        return "Draw";
+        console.log("Draw");
+        return 0;
     }
     if(comp === "scissors" && player === "paper"){
-        return "you lose scissors beats paper"
+        console.log("you lose scissors beats paper");
+        return 1;
     }
     if(comp === "scissors" && player === "scissors"){
-        return "Draw";
+        console.log("Draw");
+        return 0;
+        
     }else{
-        return "you win " + player + " beats " + comp;
+        console.log("you win " + player + " beats " + comp);
+        return 2;
     }
+    
 
 }
 
-let player = playerSelection("scissors");
+function score(){
 
-console.log(playRound(player, computerPlay()));
+}
+
+function game(){
+    let rounds = 0;
+    let player = 0;
+    let comp = 0;
+    while(rounds < 5){
+      let score = playRound(playerSelection(),computerPlay());
+        rounds++;
+        console.log(rounds + " of 5");
+        if(score === 2){
+            player++;
+        }else if(score === 1){
+            comp++;
+        }
+    }
+    console.log("Player points: " + player + " " + "Computer points: " +comp )
+    if(player > comp){
+        console.log(`You win with ${player} points`);
+    }else if(comp > player){
+        console.log(`You lose with ${player} points`);
+    }else{
+        console.log("Draw");
+    }
+}
+game();
+
